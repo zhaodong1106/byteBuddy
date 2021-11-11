97d08c0f0eff5c91ae1c0084f610d03b6af6be86
zhaodong <198758941@qq.com>
2021-11-11 20:42:15 +0800

    313213

--- src/main/java/com/zhaodong/byteBuddy/AgentMain.java
+++ src/main/java/com/zhaodong/byteBuddy/AgentMain.java
@@ -1,40 +1,33 @@
 package com.zhaodong.byteBuddy;
 
 import com.google.gson.Gson;
-
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.MessageToByteEncoder;
-import io.netty.handler.codec.ReplayingDecoder;
 import io.netty.util.ReferenceCountUtil;
 import net.bytebuddy.agent.builder.AgentBuilder;
-import net.bytebuddy.description.annotation.AnnotationDescription;
-import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.matcher.ElementMatchers;
 
-
-import java.lang.annotation.Annotation;
 import java.lang.instrument.Instrumentation;
-import java.nio.charset.Charset;
+import java.lang.instrument.UnmodifiableClassException;
 import java.util.Map;
 
-import static java.lang.Class.forName;
-import static net.bytebuddy.matcher.ElementMatchers.*;
+import static net.bytebuddy.matcher.ElementMatchers.nameEndsWith;
+import static net.bytebuddy.matcher.ElementMatchers.named;
 
-public class PreMain {
+public class AgentMain {
     private static Gson gson=new Gson();
 
     public static ChannelFuture cs=null;
     private  static EventLoopGroup workerGroup=null;
 
-    //JVM 首先尝试在代理类上调用以下方法
-    public static void premain(String agentArgs, Instrumentation inst) throws InterruptedException {
-        System.out.println("进入premain代理");
+
+    public static void agentmain(String agentArgs, Instrumentation inst) throws ClassNotFoundException, UnmodifiableClassException {
+        System.out.println("进入AgentMain代理");
         Map<String, String> argsMap = ArgsParse.parse(agentArgs);
         System.out.println("参数:"+gson.toJson(argsMap));
         if (!argsMap.containsKey("basePackage")){
@@ -44,42 +37,42 @@ public class PreMain {
         }
 
 
-            AgentBuilder.Transformer transformer = (builder, typeDescription, classLoader, javaModule) -> {
-                return builder
-                        .method(ElementMatchers.isAnnotatedWith(named("org.springframework.web.bind.annotation.GetMapping")
-                                .or((named("org.springframework.web.bind.annotation.PostMapping")))
-                                .or((nameEndsWith("PrintLog")))
-                        ))
+        AgentBuilder.Transformer transformer = (builder, typeDescription, classLoader, javaModule) -> {
+            return builder
+                    .method(ElementMatchers.isAnnotatedWith(named("org.springframework.web.bind.annotation.GetMapping")
+                            .or((named("org.springframework.web.bind.annotation.PostMapping")))
+                            .or((nameEndsWith("PrintLog")))
+                    ))
 //                        .method(ElementMatchers.declaresMethod(ElementMatchers.isAnnotatedWith(ElementMatchers.nameContains("")))) // 拦截任意方法
-                        .intercept(MethodDelegation.to(MonitorMethod.class)); // 委托
+                    .intercept(MethodDelegation.to(MonitorMethod.class)); // 委托
 
-            };
+        };
 
-            new AgentBuilder
-                    .Default()
-                    .type(ElementMatchers.nameStartsWith(argsMap.get("basePackage")))
+        new AgentBuilder
+                .Default()
+                .type(ElementMatchers.nameStartsWith(argsMap.get("basePackage")))
 //                .type(ElementMatchers.hasAnnotation(ElementMatchers.annotationType(ElementMatchers.nameContains("RestController"))
 //                        .or(ElementMatchers.annotationType(ElementMatchers.nameContains("Controller")))) )
-                    // 指定需要拦截的类 "cn.bugstack.demo.test"
+                // 指定需要拦截的类 "cn.bugstack.demo.test"
 //                .type()  // 指定需要拦截的类 "cn.bugstack.demo.test"
-                    .transform(transformer)
-                    .installOn(inst);
-           new Thread(()->{
-               try {
-                   nettyConnection();
-                   cs.channel().closeFuture().sync();
-               } catch (InterruptedException e) {
-                   e.printStackTrace();
-               }
-           }).start();
+                .transform(transformer)
+                .installOn(inst);
+        new Thread(()->{
+            try {
+                nettyConnection();
+                cs.channel().closeFuture().sync();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
 
-             Runtime.getRuntime().addShutdownHook(new Thread(()->{
-                 System.out.println("关闭netty");
+        Runtime.getRuntime().addShutdownHook(new Thread(()->{
+            System.out.println("关闭netty");
 
-                 if(!workerGroup.isShuttingDown()){
-                     workerGroup.shutdownGracefully();
-                 }
-             }));
+            if(!workerGroup.isShuttingDown()){
+                workerGroup.shutdownGracefully();
+            }
+        }));
 
     }
 
@@ -96,7 +89,7 @@ public class PreMain {
             @Override
             public void initChannel(SocketChannel ch)
                     throws Exception {
-                ch.pipeline().addLast(new ClientHandler());
+                ch.pipeline().addLast(new PreMain.ClientHandler());
             }
         });
 
@@ -105,9 +98,9 @@ public class PreMain {
 
     }
 
-        //如果代理类没有实现上面的方法，那么 JVM 将尝试调用该方法
-        public static void premain (String agentArgs){
-        }
+    //如果代理类没有实现上面的方法，那么 JVM 将尝试调用该方法
+    public static void premain (String agentArgs){
+    }
 
     public static class ClientHandler extends ChannelInboundHandlerAdapter {
 
